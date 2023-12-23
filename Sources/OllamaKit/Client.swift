425999a794c453af4b53121d903c8894e0f167ab
Nathan Borror <nathan@nathanborror.com>
2023-12-23 09:34:15 -0800

    ugh

--- Sources/OllamaKit/Client.swift
+++ Sources/OllamaKit/Client.swift
@@ -4,8 +4,8 @@ public final class OllamaClient {
     
     let host: URL
     
-    public init(host: String = "127.0.0.1:8080") {
-        self.host = URL(string: "\(host)/api")!
+    public init(url: URL) {
+        self.host = url.appending(path: "api")
     }
     
     // Generate
