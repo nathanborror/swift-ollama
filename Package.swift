8c255c94a136ea2d636b166dff20b85e5f0ff2e6
Nathan Borror <nathan@nathanborror.com>
2024-01-07 21:41:15 -0800

    rename

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
         .library(name: "Ollama", targets: ["Ollama"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathanborror/swift-sharedkit", branch: "main"),
+        .package(url: "https://github.com/nathanborror/swift-shared-kit", branch: "main"),
     ],
     targets: [
         .target(name: "Ollama", dependencies: [
-            .product(name: "SharedKit", package: "swift-sharedkit"),
+            .product(name: "SharedKit", package: "swift-shared-kit"),
         ]),
         .testTarget(name: "OllamaTests", dependencies: ["Ollama"]),
     ]
